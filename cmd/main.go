4247f1511a651d3dfb569571ea7909e82dbfaf68
tzh <2735604537@qq.com>
2025-12-18 20:59:43 +0800

    refactor: extract login module from main.go and restructure project

--- cmd/main.go
+++ cmd/main.go
@@ -1,35 +1,22 @@
 package main
 
 import (
-	config "AutoLogin/internal"
-	"encoding/json"
-	"errors"
+	"autologin/internal/config"
+	"autologin/internal/login"
 	"flag"
-	"io"
 	"log"
 	"net/http"
 	"net/http/cookiejar"
-	url2 "net/url"
-	"regexp"
-	"strings"
-)
-
-var (
-	cfgPath string
-)
-
-var (
-	ErrRedirectURLNotFound = errors.New("redirect URL not found in response body")
-	ErrHTTPStatusNotOK     = errors.New("http status not OK")
 )
 
 func init() {
-	flag.StringVar(&cfgPath, "config", "config/config.toml", "config file path")
+	flag.StringVar(&config.CfgPath, "config", "config/config.toml", "config file path")
 	flag.Parse()
 }
 
 func main() {
-	cfg, err := config.LoadConfig(cfgPath)
+
+	cfg, err := config.LoadConfig(config.CfgPath)
 	if err != nil {
 		log.Fatal("config read failed: %v", err)
 	}
@@ -38,102 +25,12 @@ func main() {
 	client := &http.Client{
 		Jar: jar,
 	}
+	err = login.CheckNetworkConnectivityWithURL(&cfg)
+	if err == login.ErrHTTPStatusNotOK {
+		queryString, err := login.FetchQueryString(client, &cfg)
+		err = login.AuthenticateWithCredentials(client, &cfg, queryString)
+		log.Println(err)
 
-	queryString, err := fetchQueryString(client, &cfg)
-	if err == ErrRedirectURLNotFound {
-		err = CheckNetworkConnectivityWithURL(&cfg)
-		if err != nil {
-			log.Fatal(err)
-		}
 	}
 
-	err = login(client, &cfg, queryString)
-
-}
-
-func CheckNetworkConnectivityWithURL(cfg *config.Config) error {
-	resp, err := http.Get(cfg.API.TestURL)
-	if err != nil {
-		return err
-	}
-	if resp.StatusCode != http.StatusOK {
-		log.Println(resp.StatusCode)
-		return ErrHTTPStatusNotOK
-	}
-	log.Println(resp.StatusCode)
-	return nil
-}
-
-func fetchQueryString(client *http.Client, cfg *config.Config) (string, error) {
-	resp, err := client.Get(cfg.API.BaseURL)
-	if err != nil {
-		log.Printf("request to %s failed: %v", cfg.API.BaseURL, err)
-		return "", err
-	}
-
-	content, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Printf("failed to read response body: %v", err)
-		return "", err
-	}
-
-	re := regexp.MustCompile(`location\.href='([^']+)'`)
-	matches := re.FindStringSubmatch(string(content))
-	if len(matches) < 2 {
-		log.Printf("redirect URL not found in response body")
-		return "", ErrRedirectURLNotFound
-	}
-
-	redirectURL := matches[1]
-	log.Printf("redirect URL extracted: %s", redirectURL)
-
-	resp, err = client.Get(redirectURL)
-	if err != nil {
-		log.Printf("request to redirect URL failed: %v", err)
-		return "", err
-	}
-
-	queryString := resp.Request.URL.RawQuery
-	log.Printf("raw query string: %s", queryString)
-	return queryString, nil
-
-}
-
-type LoginResponse struct {
-	UserIndex         string      `json:"userIndex"`
-	Result            string      `json:"result"`
-	Message           string      `json:"message"`
-	Forwordurl        interface{} `json:"forwordurl"`
-	KeepaliveInterval int         `json:"keepaliveInterval"`
-	CasFailErrString  interface{} `json:"casFailErrString"`
-	ValidCodeURL      string      `json:"validCodeUrl"`
-}
-
-func login(client *http.Client, cfg *config.Config, queryString string) error {
-	data := url2.Values{}
-	data.Set("userId", cfg.Auth.UserID)
-	data.Set("password", cfg.Auth.Password)
-	data.Set("service", url2.QueryEscape(cfg.Auth.Service))
-	data.Set("queryString", url2.QueryEscape(queryString))
-	data.Set("operatorPwd", "")
-	data.Set("operatorUserId", "")
-	data.Set("validcode", "")
-	data.Set("passwordEncrypt", "false")
-
-	resp, err := client.Post(cfg.API.LoginURL, "application/x-www-form-urlencoded; charset=UTF-8", strings.NewReader(data.Encode()))
-	if err != nil {
-		log.Printf("login request failed: %v", err)
-		return err
-	}
-
-	content, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Printf("failed to read login response body: %v", err)
-		return err
-	}
-	log.Printf("login response body: %s", content)
-	var loginResp LoginResponse
-	json.Unmarshal(content, &loginResp)
-	log.Printf("login response: %+v", loginResp.Result)
-	return nil
 }
--- go.mod
+++ go.mod
@@ -1,11 +1,5 @@
-module AutoLogin
+module autologin
 
-go 1.23.0
+go 1.25.5
 
-toolchain go1.24.11
-
-require (
-	github.com/BurntSushi/toml v1.5.0 // indirect
-	github.com/kardianos/service v1.2.4 // indirect
-	golang.org/x/sys v0.34.0 // indirect
-)
+require github.com/BurntSushi/toml v1.6.0 // indirect
